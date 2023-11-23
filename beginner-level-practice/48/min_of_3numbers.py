f15d54a971be988d4bf8be6fcdade27b5c8e4055
rafsun-riad <mruhaquer@gmail.com>
2023-11-23 23:43:24 +0600

    day 7

---
+++ beginner-level-practice/48/min_of_3numbers.py
@@ -0,0 +1,11 @@
+a = 1
+b = 3
+c = 4
+
+
+if (a <= b) and (a <= c):
+    print(a)
+elif (b <= a) and (b <= c):
+    print(b)
+else:
+    print(c)
---
+++ beginner-level-practice/50/compare_3_numbers.py
@@ -0,0 +1,8 @@
+a = 1
+b = 2
+c = 1
+
+if a == b and b == c:
+    print("Equal")
+else:
+    print("Not Equal")
